2a177de675907d87e7a45367bb1941928a37461f
pattjoshi <omprakashpattjoshi123@gmail.com>
2022-06-11 17:25:50 +0530

    Adding creator data

---
+++ src/component/CreatorData.jsx
@@ -0,0 +1,67 @@
+const CreatorData = [
+    {
+      id: 1,
+      userName: "CarryMinati",
+      profileUrl:
+        "https://oprice.in/wp-content/uploads/2020/11/Carryminati-Net-Worth-2020.jpg",
+      profession: "youtuber",
+    },
+    {
+      id: 2,
+      userName: "BB Ki Vines",
+      profileUrl:
+        "https://www.iwmbuzz.com/wp-content/uploads/2020/12/bb-ki-vines-special-your-golden-opportunity-to-get-your-question-answered-by-the-amazing-bhuvan-bam-920x508.jpeg",
+      profession: "Actor",
+    },
+    {
+      id: 3,
+      userName: "Rahul Subramanian",
+      profileUrl:
+        "https://www.scrolldroll.com/wp-content/uploads/2020/11/Rahul-Subramanian-Best-Stand-Up-Comedian-India-YouTube.png",
+      profession: "Comedian",
+    },
+    {
+      id: 4,
+      userName: "Anubhav Bassi",
+      profileUrl:
+        "https://www.scrolldroll.com/wp-content/uploads/2020/11/Abhinav-Singh-Bassi-Best-Stand-Up-Comedian-India-YouTube.png",
+      profession: "Comedian",
+    },
+    {
+      id: 5,
+      userName: "Álvaro Morte",
+      profileUrl:
+        "https://celebsupdate.com/wp-content/uploads/2021/06/pic-01-3.jpg",
+      profession: "Actor",
+    },
+    {
+      id: 6,
+      userName: "Pedro Alonso",
+      profileUrl:
+        "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTdxUO8fb8idHHW4dHfPa2cc76UaKJ3yA0ojiqmAAfUoZB5rPp6",
+      profession: "Actor",
+    },
+    {
+      id: 7,
+      userName: "Clara Alvarado",
+      profileUrl:
+        "http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcRrg-cGUreBRdwyAlxCzUPGudICj6zgqMs-OVLwAHjPta2dVtie-sjQDsU6PxhKvV-M",
+      profession: "Actress",
+    },
+    {
+      id: 8,
+      userName: "Johnny Depp",
+      profileUrl:
+        "https://i.guim.co.uk/img/media/5827279817bdc9e8c7b655c8f6798c2d53ca1170/0_21_3000_1801/master/3000.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=79479e545975b375a2bebfbd2f90df8c",
+      profession: "Actor",
+    },
+    {
+      id: 9,
+      userName: "Emma stone",
+      profileUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVLO5tg9d9KiR4Sb7C-04WwkaS7EuSYdM4hA&usqp=CAU",
+      profession: "American actress",
+    },
+  ];
+  
+  export default CreatorData;
